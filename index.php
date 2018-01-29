e919455d22fbc7c3be03cf9d064864626440cf9a
tasabina <32531721+tasabina@users.noreply.github.com>
2018-01-30 01:09:48 +1300

    index.php
    First file in this system

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+require_once 'connection.php';
+
+$link = mysqli_connect($host, $user, $pswd, $database);
+
+
+/* проверка соединения */
+if (mysqli_connect_errno()) {
+    printf("Не удалось подключиться: %s\n", mysqli_connect_error());
+    exit();
+}
+
+$query = "SELECT * FROM `aboutme` WHERE 1";
+
+/* execute multi query */
+if (mysqli_multi_query($link, $query)) {
+    do {
+        /* store first result set */
+        if ($result = mysqli_store_result($link)) {
+            while ($row = mysqli_fetch_row($result)) {
+                printf("%s\n", $row[0], $row[2]);
+
+            }
+            mysqli_free_result($result);
+        }
+        /* print divider */
+        if (mysqli_more_results($link)) {
+            printf("-----------------\n");
+        }
+    } while (mysqli_next_result($link));
+}
+
+/* close connection */
+mysqli_close($link);
+?>
